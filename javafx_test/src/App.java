a6c6d2d86cb5bc2e1cbd9cdbe506691940315b1f
Cine-Pixel <ikaumphriani@gmail.com>
2022-06-03 14:21:33 +0400

    final prep

---
+++ javafx_test/src/App.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ javafx_test/src/MainSceneController.java
@@ -0,0 +1,99 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class MainSceneController {
+
+    @FXML
+    private Button buttonAdd;
+
+    @FXML
+    private TextField fieldAddress;
+
+    @FXML
+    private TextField fieldEmail;
+
+    @FXML
+    private TextField fieldName;
+
+    @FXML
+    private TableView<UniItem> tableItems;
+
+    @FXML
+    private TableColumn<UniItem, String> colEmail;
+
+    @FXML
+    private TableColumn<UniItem, String> colId;
+
+    @FXML
+    private TableColumn<UniItem, String> colName;
+
+    @FXML
+    private TableColumn<UniItem, String> colAddress;
+
+    @FXML
+    void buttonAddClicked(ActionEvent event) {
+        String url = "jdbc:mariadb://localhost:3306/university";
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection(url, "root", "");
+            PreparedStatement prepStmt = conn.prepareStatement("INSERT INTO uni(name, address, email) VALUES(?, ?, ?)");
+            prepStmt.setString(1, fieldName.getText());
+            prepStmt.setString(2, fieldAddress.getText());
+            prepStmt.setString(3, fieldEmail.getText());
+            prepStmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void buttonLoadClicked(ActionEvent event) {
+        String url = "jdbc:mariadb://localhost:3306/university";
+        Connection conn;
+        tableItems.getItems().clear();
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+
+        try {
+            conn = DriverManager.getConnection(url, "root", "");
+            String query = "SELECT * from uni";
+            java.sql.Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                String id = Integer.toString(rs.getInt("id"));
+                String name = rs.getString("name");
+                String address = rs.getString("address");
+                String email = rs.getString("email");
+
+                System.out.println(id);
+                System.out.println(name);
+                System.out.println(address);
+                System.out.println(email);
+
+                UniItem row = new UniItem();
+                row.setId(id);
+                row.setName(name);
+                row.setAddress(address);
+                row.setEmail(email);
+                tableItems.getItems().add(row);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
